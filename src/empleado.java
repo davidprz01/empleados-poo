6c895091e7f7bb8f31388e5b296f65562ae5008e
JuanD-777 <juandicastrillon2206@gmail.com>
2026-03-02 09:53:19 -0500

    Nuevo metodo actulizarCargo fue agregado

--- src/empleado.java
+++ src/empleado.java
@@ -1,7 +1,17 @@
 public class Empleado { 
 private String nombre; 
-private int salario; 
-public Empleado(String nombre, int salario) { 
-this.nombre = nombre; 
+private int salario;
+private String cargo;
+public Empleado(String nombre, int salario, String cargo) {
+this.nombre = nombre;
+this.cargo = cargo;
 this.salario = salario; 
+}
+
+
+// Actulizar cargo
+public void actualizarCargo(String nuevoCargo) {
+   cargo = nuevoCargo;
+   System.out.println("el cargo fue actualizado a: "+ cargo);
+  }
 }
