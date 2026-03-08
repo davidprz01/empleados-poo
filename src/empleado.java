61c2a5fe591c15e83288be20da21e361d18d5c07
Saimon1704-VT <simon.vt4807@gmail.com>
2026-03-08 18:04:17 -0500

    funcion aumentar salario

--- src/empleado.java
+++ src/empleado.java
@@ -5,6 +5,7 @@ public Empleado(String nombre, int salario) {
 this.nombre = nombre;
 this.salario = salario;
 }
+// funcion aumentar salario
     public void aumentarSalario(int aumento) {
         salario += aumento;
     }
