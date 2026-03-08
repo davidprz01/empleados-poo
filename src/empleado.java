c92938949725694bc93c7ca9e98bc6a2b789b557
Juand.C <juandicastrillon2206@gmail.com>
2026-03-08 18:19:45 -0500

    Añadir cambio empleado

--- src/empleado.java
+++ src/empleado.java
@@ -1,17 +1,17 @@
-public class Empleado { 
-private String nombre; 
-private int salario;
-private String cargo;
-public Empleado(String nombre, int salario, String cargo) {
-this.nombre = nombre;
-this.cargo = cargo;
-this.salario = salario; 
-}
+public class Empleado {
+    private String nombre;
+    private int salario;
+    private String cargo;
+    public Empleado(String nombre, int salario, String cargo) {
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
 
 
-// Actulizar cargo
-public void actualizarCargo(String nuevoCargo) {
-   cargo = nuevoCargo;
-   System.out.println("el cargo fue actualizado a: "+ cargo);
-  }
+    // Actulizar cargo
+    public void actualizarCargo(String nuevoCargo) {
+        cargo = nuevoCargo;
+        System.out.println("el cargo fue actualizado a: "+ cargo);
+    }
 }
