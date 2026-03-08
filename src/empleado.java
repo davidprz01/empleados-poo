50d4e930e1e905fd7e7e8a3d7afdc22516d75748
KVH14 <karlavargashernandez1596@gmail.com>
2026-03-08 17:58:03 -0500

    Metodo de mostrar Datos

--- src/empleado.java
+++ src/empleado.java
@@ -1,7 +1,23 @@
-public class Empleado { 
-private String nombre; 
-private int salario; 
-public Empleado(String nombre, int salario) { 
-this.nombre = nombre; 
-this.salario = salario; 
+public class Empleado {
+    private String nombre;
+    private int salario;
+
+    public Empleado(String nombre, int salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    /* MÉTODO2:mostrarDatos*/
+    public void mostrarDatos() {
+        System.out.println("=== DATOS DEL EMPLEADO ===");
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Edad: " + getEdad());
+        System.out.println("Salario base: " + salarioBase);
+        System.out.println("Horas extras: " + horasExtras);
+        System.out.println("Valor por hora extra: " + valorHoraExtra);
+        System.out.println("Salario total calculado: " + calcularSalario());
+    }
 }
+
+
+
