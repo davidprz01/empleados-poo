d00e516e24cf8eb680785fc2b76273370ffbcc7d
Saimon1704-VT <simon.vt4807@gmail.com>
2026-03-08 18:02:35 -0500

    Agregado metodo aumentarSalario

--- src/empleado.java
+++ src/empleado.java
@@ -1,7 +1,11 @@
-public class Empleado { 
-private String nombre; 
-private int salario; 
-public Empleado(String nombre, int salario) { 
-this.nombre = nombre; 
-this.salario = salario; 
+public class Empleado {
+private String nombre;
+private int salario;
+public Empleado(String nombre, int salario) {
+this.nombre = nombre;
+this.salario = salario;
+}
+    public void aumentarSalario(int aumento) {
+        salario += aumento;
+    }
 }
